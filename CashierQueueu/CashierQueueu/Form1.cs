c996b8fb67e85356a6cfefc49df954cf8a6dfbbd
tine0328 <144678351+tine0328@users.noreply.github.com>
2023-09-18 19:55:45 +0800

    Add files via upload

---
+++ CashierQueueu/CashierQueueu/Form1.cs
@@ -0,0 +1,22 @@
+using System.Drawing.Text;
+
+namespace CashierQueueu
+{
+    public partial class Form1 : Form
+    {
+        private CashierClass cashier;
+        public Form1()
+        {
+            InitializeComponent();
+            cashier = new CashierClass();
+            new CashierWindowQueueForm().Show();
+        }
+
+        private void btnCashier_Click(object sender, EventArgs e)
+        {
+            lblQueue.Text = cashier.CashierGeneratedNumber("P - ");
+            CashierClass.getNumberInQueue = lblQueue.Text;
+            CashierClass.CashierQueue.Enqueue(CashierClass.getNumberInQueue);
+        }
+    }
+}
